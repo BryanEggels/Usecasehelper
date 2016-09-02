525dd984b6f954ed20cece083a224fd13b8cad4d
Bryan Eggels <356103@student.fontys.nl>
2016-09-02 23:43:14 +0200

    Actor class

--- Usecase/Actor.cs
+++ Usecase/Actor.cs
@@ -18,7 +18,7 @@ namespace Usecase
 
         private void DrawHead(Graphics g, Point p1)
         {
-            Rectangle rec = new Rectangle(p1.X -10, p1.Y-20, 20, 20);
+            Rectangle rec = new Rectangle(p1.X - 10, p1.Y - 20, 20, 20);
             g.DrawEllipse(streeppen, rec);
         }
         private void DrawArms(Graphics g, Point p1)
@@ -30,7 +30,7 @@ namespace Usecase
         private void DrawLegs(Graphics g, Point p1)
         {
             Point legpoint1 = new Point(p1.X, p1.Y + 30);
-            Point legpoint2 = new Point(p1.X + 10, p1.Y+45);
+            Point legpoint2 = new Point(p1.X + 10, p1.Y + 45);
             g.DrawLine(streeppen, legpoint1, legpoint2);
             legpoint2.X = legpoint2.X - 20;
             g.DrawLine(streeppen, legpoint1, legpoint2);
